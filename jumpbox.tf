7eea56e97c81c02d6bea1fa09cfb723e16babf9b
r2oro <artupior@gmail.com>
2025-06-04 11:12:12 +0200

    split instances

---
+++ jumpbox.tf
@@ -0,0 +1,58 @@
+# This Terraform configuration sets up a jumpbox instance in AWS.
+
+resource "aws_key_pair" "sdm-hwrk_key" {
+    key_name   = "sdm-hwrk-key"
+    public_key = file("~/.ssh/arpi.pub")
+    tags = {
+        Name = "sdm-hwrk-key"
+    }
+}
+
+resource "aws_security_group" "jumpbox_sg" {
+    vpc_id = aws_vpc.sdm-hwrk-vpc.id
+    name = "sdm-hwrk-jumpbox-sg"
+    description = "Security group for the jumpbox instance"
+    
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Allow SSH access"
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "Allow all outbound traffic"
+    }
+    tags = {
+        Name = "sdm-hwrk-jumpbox-sg"
+    }
+}
+
+resource "aws_eip" "jumpbox_eip" {
+    tags = {
+        Name = "sdm-hwrk-jumpbox-eip"
+    } 
+}
+
+resource "aws_instance" "jumpbox" {
+    ami =  data.aws_ami.linux_ami.id
+    associate_public_ip_address = true
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.public_subnet.id
+    vpc_security_group_ids =  [aws_security_group.jumpbox_sg.id]
+    key_name = aws_key_pair.sdm-hwrk_key.key_name
+    user_data = templatefile("centos-jumpbox.tpl.sh", {})
+    user_data_replace_on_change = true
+    tags = {
+        Name = "sdm-hwrk-jumpbox"
+    }
+}
+
+resource "aws_eip_association" "jumpbox_eip_association" {
+    instance_id = aws_instance.jumpbox.id
+    allocation_id = aws_eip.jumpbox_eip.id
+}
--- private-instance.tf
+++ private-instance.tf
@@ -1,61 +1,4 @@
-# Jumpbox configuration
-
-resource "aws_key_pair" "sdm-hwrk_key" {
-    key_name   = "sdm-hwrk-key"
-    public_key = file("~/.ssh/arpi.pub")
-    tags = {
-        Name = "sdm-hwrk-key"
-    }
-}
-
-resource "aws_security_group" "jumpbox_sg" {
-    vpc_id = aws_vpc.sdm-hwrk-vpc.id
-    name = "sdm-hwrk-jumpbox-sg"
-    description = "Security group for the jumpbox instance"
-    
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Allow SSH access"
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-        description = "Allow all outbound traffic"
-    }
-    tags = {
-        Name = "sdm-hwrk-jumpbox-sg"
-    }
-}
-
-resource "aws_eip" "jumpbox_eip" {
-    tags = {
-        Name = "sdm-hwrk-jumpbox-eip"
-    } 
-}
-
-resource "aws_instance" "jumpbox" {
-    ami =  data.aws_ami.linux_ami.id
-    associate_public_ip_address = true
-    instance_type = "t2.micro"
-    subnet_id = aws_subnet.public_subnet.id
-    vpc_security_group_ids =  [aws_security_group.jumpbox_sg.id]
-    key_name = aws_key_pair.sdm-hwrk_key.key_name
-    tags = {
-        Name = "sdm-hwrk-jumpbox"
-    }
-}
-
-resource "aws_eip_association" "jumpbox_eip_association" {
-    instance_id = aws_instance.jumpbox.id
-    allocation_id = aws_eip.jumpbox_eip.id
-}
-
-# Private instance configuration
+# This Terraform configuration sets up a private instance in AWS.
 
 resource "aws_security_group" "private_sg" {
     vpc_id = aws_vpc.sdm-hwrk-vpc.id
@@ -89,7 +32,7 @@ resource "aws_instance" "private_instance" {
     subnet_id = aws_subnet.private_subnet.id
     vpc_security_group_ids =  [aws_security_group.private_sg.id]
     key_name = aws_key_pair.sdm-hwrk_key.key_name
-    user_data = templatefile("private_instance.tpl.sh", {
+    user_data = templatefile("centos-sdm.tpl.sh", {
         target_user = "ec2-user"
         sshca       = data.sdm_ssh_ca_pubkey.ssh_pubkey_query.public_key
     })
