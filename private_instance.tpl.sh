c401747cb5fb1f7ed6236cda90ec9fd51df2359f
r2oro <artupior@gmail.com>
2025-06-04 10:51:14 +0200

    corrected permissions

--- private_instance.tpl.sh
+++ private_instance.tpl.sh
@@ -10,7 +10,7 @@ SSHD_CONFIG="/etc/ssh/sshd_config"
 
 echo "Creating SSHCA as $SDM_CA" | logger -t sdminstall
 echo "${sshca}" >  $SDM_CA
-chmod 600 $SDM_CA
+chmod 400 $SDM_CA
 chown root:root $SDM_CA
 
 echo "Reconfiguring SSHD" | logger -t sdminstall
@@ -19,11 +19,11 @@ echo "AuthorizedPrincipalsFile $SDM_USERS/%u" >> $SSHD_CONFIG
 
 echo "Enabling $TARGET_USER to login using SSH CA" | logger -t sdminstall
 mkdir $SDM_USERS
-chmod 700 $SDM_USERS
+chmod 711 $SDM_USERS # Ensure directory is accessible to $TARGET_USER (sshd switches to $TARGET_USER)
 echo "strongdm" > $TARGET_USER_CONF
-chmod 600 $TARGET_USER_CONF
+chmod 644 $TARGET_USER_CONF # Ensure file is accessible to $TARGET_USER (sshd switches to $TARGET_USER)
 
 echo "Restarting SSHD" | logger -t sdminstall
-systemctl restart ssh
+systemctl restart sshd
 
 echo "StrongDM target configuration done" | logger -t sdminstall
